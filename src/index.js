6ef6067567454dc3a27fc15f9c77f3c045d499f4
ABDIRIZACK <abdirizack466@gmail.com>
2026-02-16 09:44:26 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#create-task-form');
+  const input = document.querySelector('#new-task-description');
+  const tasks = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevent page reload
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    tasks.appendChild(li);
+    input.value = ''; // clear input
+  });
+});
+
